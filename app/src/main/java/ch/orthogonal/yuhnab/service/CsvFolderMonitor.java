1be72218bcc534761295cf568881cfad0f4c9435
Matthias Weber <matthias.weber@etik.com>
2025-07-06 18:16:31 +0200

    Replace while(True) with proper shutdown hook

--- app/src/main/java/ch/orthogonal/yuhnab/service/CsvFolderMonitor.java
+++ app/src/main/java/ch/orthogonal/yuhnab/service/CsvFolderMonitor.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class CsvFolderMonitor {
     private static final Logger logger = LogManager.getLogger(CsvFolderMonitor.class);
@@ -18,12 +19,30 @@ public class CsvFolderMonitor {
 
 
     public void run() throws IOException, InterruptedException {
+        AtomicBoolean isRunning = new AtomicBoolean(false);
         WatchService watchService = FileSystems.getDefault().newWatchService();
         monitoredFolder.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
         logger.info("Watching folder: {}", monitoredFolder.toAbsolutePath());
+        logger.info("Press Ctrl+C to exit");
+        isRunning.set(true);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Shutting down...");
+            isRunning.set(false);
+            try {
+                watchService.close();
+            } catch (IOException ignored) {
+            }
+        }));
 
-        while (true) {
-            WatchKey key = watchService.take();
+
+        while (isRunning.get()) {
+            WatchKey key;
+            try {
+                key = watchService.take();
+            } catch (InterruptedException e) {
+                logger.warn(e.getMessage());
+                continue;
+            }
             for (WatchEvent<?> event : key.pollEvents()) {
                 if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
                     Path filename = (Path) event.context();
