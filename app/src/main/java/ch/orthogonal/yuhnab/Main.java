ea5b8e19c26e760ff5a7437bf4853d14a02d1fb5
Matthias Weber <matthias.weber@etik.com>
2025-07-08 05:53:51 +0200

    Cleanup of main exceptions

--- app/src/main/java/ch/orthogonal/yuhnab/Main.java
+++ app/src/main/java/ch/orthogonal/yuhnab/Main.java
@@ -6,13 +6,11 @@ import ch.orthogonal.yuhnab.command.CommandFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         try {
             Command command = CommandFactory.createCommand(args);
             command.execute();
